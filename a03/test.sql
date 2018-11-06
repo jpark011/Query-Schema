00dccba6a033d14b08a4a17eaf5c7ff151caa6f2
Jay Park <wngt3319@gmail.com>
2018-11-06 09:31:43 -0500

    [a02] init version

--- a03/test.sql
+++ a03/test.sql
@@ -1,25 +1,34 @@
+WITH unionPub AS (
+    (SELECT pubid, year, NULL AS volume, NULL AS number, NULL AS publisher, NULL AS appearsin, NULL AS startpage, NULL AS endpage
+    FROM proceedings)
+    UNION
+    (SELECT pubid, year, volume, number, NULL AS publisher, NULL AS appearsin, NULL AS startpage, NULL AS endpage
+    FROM journal)
+    UNION
+    (SELECT pubid, year, NULL AS volume, NULL AS number, publisher, NULL AS appearsin, NULL AS startpage, NULL AS endpage
+    FROM book)
+    UNION
+    (SELECT pubid, NULL AS year, NULL AS volume, NULL AS number, NULL AS publisher, appearsin, startpage, endpage
+    FROM article)
+)
 SELECT Au.name name, Pu.title title, Pu.pubid pubid \
+        P.year year, P.volume volume, P.number number, P.publisher publisher,
+        P.appearsin appearsin, P.startpage startpage, P.endpage endpage
 FROM author Au \
     JOIN wrote W \
         ON W.aid = Au.aid \
     JOIN publication Pu \
         ON Pu.pubid = W.pubid \
-    LEFT JOIN proceedings Pr \
-        ON Pr.pubid = Pu.pubid \
-    LEFT JOIN journal J \
-        ON J.pubid = Pu.pubid \
-    LEFT JOIN book B \
-        ON B.pubid = Pu.pubid \
-    LEFT JOIN article A \ 
-        ON A.pubid = Pu.pubid \
+    JOIN unionPub P
+        ON P.pubid = Pu.pubid \
 WHERE Au.name LIKE "Peter Bumbulis" 
 
--- SELECT Au.name name
--- FROM book B \
---     JOIN publication Pu
---         ON Pu.pubid = B.pubid
---     LEFT JOIN wrote W
---         ON W.pubid = Pu.pubid
---     JOIN author Au
---         ON Au.aid = W.aid
--- WHERE B.pubid = :pubid
+SELECT Au.name name
+FROM book B \
+    JOIN publication Pu
+        ON Pu.pubid = B.pubid
+    LEFT JOIN wrote W
+        ON W.pubid = Pu.pubid
+    JOIN author Au
+        ON Au.aid = W.aid
+WHERE B.pubid = :pubid
